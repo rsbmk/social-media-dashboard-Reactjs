acfefdb2a20e843402bed6db24d88157af2334ac
Roberto Bocio Melo <bociomelors@gmail.com>
2020-08-26 03:05:17 -0400

    Updates

---
+++ precache-manifest.b3153e29f802de644cf332884f687a04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c721fefebd23d2aed5ff62cc4eb53dd4",
+    "url": "/social-media-dashboard-Reactjs/index.html"
+  },
+  {
+    "revision": "b1bc06d5ea28dddc3b90",
+    "url": "/social-media-dashboard-Reactjs/static/css/main.af13135b.chunk.css"
+  },
+  {
+    "revision": "446ad1819deedf60c00f",
+    "url": "/social-media-dashboard-Reactjs/static/js/2.8c74e645.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/social-media-dashboard-Reactjs/static/js/2.8c74e645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1bc06d5ea28dddc3b90",
+    "url": "/social-media-dashboard-Reactjs/static/js/main.a152d2f9.chunk.js"
+  },
+  {
+    "revision": "24e0390ca6466aa82ca9",
+    "url": "/social-media-dashboard-Reactjs/static/js/runtime-main.b0655c22.js"
+  }
+]);
